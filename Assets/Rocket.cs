ea5259188299a7d1a417c94962f8289023a03c5f
GwenRspl <gwenaelle.rispal@gmail.com>
2020-03-24 15:39:14 +0100

    add sound

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -2,34 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rocket : MonoBehaviour { 
+public class Rocket : MonoBehaviour {
 
     Rigidbody rigidBody;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         ProcessInput();
     }
 
-    private void ProcessInput(){
-        if (Input.GetKey(KeyCode.Space))
-        {
+    private void ProcessInput() {
+        if (Input.GetKey(KeyCode.Space)) {
             rigidBody.AddRelativeForce(Vector3.up);
+
+            if (!audioSource.isPlaying) {
+                audioSource.Play();
+            }
         }
 
-        if (Input.GetKey(KeyCode.A))
-        {
-            print("Rotating left");
-        } else if (Input.GetKey(KeyCode.D))
-        {
-            print("Rotating right");
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            audioSource.Stop();
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Rotate(Vector3.forward);
+        } else if (Input.GetKey(KeyCode.D)) {
+            transform.Rotate(-Vector3.forward);
         }
 
 
